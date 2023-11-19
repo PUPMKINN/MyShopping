01a5ebefe214ece6e4a1a40978a83198482cdc72
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-11-19 10:46:47 +0700

    login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home');
+  res.render('home/home');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login/login');
 });
 
 module.exports = router;
