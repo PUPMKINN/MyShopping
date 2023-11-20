b4597fba36b5b93747104e3f8dd6847e56c96226
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-11-20 20:46:41 +0700

    sss

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/javascripts')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
