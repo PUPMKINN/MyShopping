31202a5b48ee354c47e9221c6c1aa7fe8a6f8d04
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-11-20 14:00:40 +0700

    js

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(logger('dev'));
 app.use(express.json());
