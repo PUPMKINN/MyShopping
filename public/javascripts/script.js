784aeb897f3b8740128b1c306000613b766a82e6
PUPMKINN <93564591+PUPMKINN@users.noreply.github.com>
2023-12-04 00:14:21 +0700

    product

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,13 +14,10 @@ const openData = function () {
 for (let i = 0; i < triggerOpen.length; i++) {
     let currentId = triggerOpen[i].dataset.target,
         targetEl = document.querySelector(`#${currentId}`)
-
         const openData = function() {
             targetEl.classList.remove('active');
             overlay.classList.remove('active');
         }
-
-    
     triggerOpen[i].addEventListener('click', function () {
         if (targetEl) {
             targetEl.classList.add('active');
@@ -35,37 +32,8 @@ for (let i = 0; i < triggerOpen.length; i++) {
     overlay.forEach(function (overlayItem) {
         overlayItem.addEventListener('click', openData);
     });
-    // overlay.addEventListener('click', openData)
+    // overlay.addEventListener('click', openData);
 }
-
-// const triggerOpen = document.querySelectorAll('[trigger-button]');
-// const triggerClose = document.querySelectorAll('[close-button]');
-// const overlay = document.querySelectorAll('[data-overlay]');
-// let targetEl;
-
-// const openData = function () {
-//     if (targetEl) {
-//         targetEl.classList.remove('active');
-//         overlay.forEach(item => item.classList.remove('active'));
-//     }
-// };
-
-// for (let i = 0; i < triggerOpen.length; i++) {
-//     let currentId = triggerOpen[i].dataset.target;
-//     targetEl = document.querySelector(`#${currentId}`);
-
-//     triggerOpen[i].addEventListener('click', function () {
-//         if (targetEl) {
-//             targetEl.classList.add('active');
-//             overlay.forEach(item => item.classList.add('active'));
-//         }
-//     });
-
-//     targetEl.querySelector('[close-button]').addEventListener('click', openData);
-//     overlay.forEach(function (overlayItem) {
-//         overlayItem.addEventListener('click', openData);
-//     });
-// }
 
 
 
