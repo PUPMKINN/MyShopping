581fc9ee4e7207e95a70f4d274663ad865966a6b
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2023-12-27 19:34:47 +0700

    Add model contact

---
+++ models/Contact.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+//   productId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Product', // Tham chiếu đến schema sản phẩm (Product)
+//     required: true
+//   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Tham chiếu đến schema người dùng (User)
+    required: true
+  },
+//   rating: {
+//     type: Number,
+//     required: true,
+//     min: 1,
+//     max: 5
+//   },
+  comment: {
+    type: String,
+  },
+//   title: {
+//     type: String,
+//   },
+  datePost: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
+
