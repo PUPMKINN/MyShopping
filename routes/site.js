fd85d6422946637bf29e1077e53de943812f1882
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2024-01-06 11:06:00 +0700

    add about us

--- routes/site.js
+++ routes/site.js
@@ -2,12 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const siteController = require("../controllers/SiteController");
+const User = require("../models/User");
 
 router.get("/search", siteController.search);
 router.get("/test", (req, res, next) => {
     res.render("index", {layout: 'admin'});
     //res.render();
 })
+router.get("/aboutUs", (req, res, next) => {
+    if(req.user == null){
+        res.render("aboutUs", {layout: 'guest'});
+        return;
+    }
+    const user = User.findById(req.user._id).lean();
+    const role = req.user.role;
+    res.render("aboutUs", {layout: role, user: user});
+});
 router.get("/", siteController.getHomePage);
 
 module.exports = router;
